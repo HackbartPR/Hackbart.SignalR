3efc45e65d95ac638388473792c9a3e0804a61ed
HackbartPR <cgharckbart@gmail.com>
2023-09-26 19:46:21 -0300

    chore: Removendo trechos comentados

--- ClientA/Chat.cs
+++ ClientA/Chat.cs
@@ -7,33 +7,6 @@ namespace ClientA
     /// </summary>
     public static class Chat
     {
-        //public static async Task EnviarMensagem(IHubService hubService, CancellationToken cancellationToken)
-        //{
-        //    Console.WriteLine("### Enviando Mensagem Para Todos ###");
-        //    await hubService.ManageMessage("Olá", "SendMessageToAll", cancellationToken);
-
-        //    await Task.Delay(4000, cancellationToken);
-        //    await hubService.ManageMessage("Tudo certo?", "SendMessageToAll", cancellationToken);
-
-        //    await Task.Delay(3000, cancellationToken);
-        //    Console.WriteLine("### Enviando Mensagem Somente Para Mim ###");
-        //    await hubService.ManageMessage("Meu processo foi finalizado com sucesso!", "SendMessageToCaller", cancellationToken);
-
-        //    await Task.Delay(1000, cancellationToken);
-        //    Console.WriteLine("### Entrando no Grupo01 ###");
-        //    await hubService.AddToGroup("Teste01", cancellationToken);
-
-        //    await Task.Delay(4000, cancellationToken);
-        //    Console.WriteLine("### Enviando Mensagem Somente o Grupo 'Teste01' ###");
-        //    await hubService.ManageMessage("Olá Grupo", "SendMessageToGroup", cancellationToken, "Teste01");
-
-        //    await Task.Delay(8000, cancellationToken);
-        //    await hubService.ManageMessage("Esta conversa é exclusiva deste grupo", "SendMessageToGroup", cancellationToken, "Teste01");
-
-        //    await Task.Delay(12000, cancellationToken);
-        //    await hubService.ManageMessage("Beleza", "SendMessageToGroup", cancellationToken, "Teste01");
-        //}
-
         public static async Task EnviarMensagem(IHubService hubService, CancellationToken cancellationToken)
         {
             Console.WriteLine("### Enviando Mensagem Para Todos ###");
